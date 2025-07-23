615d03d2bd5f38f854f6216eaa219d46a2382466
ayeshayusufgit <ayesha.yusuf@gmail.com>
2025-07-23 10:49:38 +0530

    changing the db details as per prod deployment

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = await mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'B@hr@inmysql786',
-  database: process.env.DB_NAME || 'loginapp',
+  host: process.env.DB_HOST || 'mysql-loginapp-db-ayesha-login-db.f.aivencloud.com',
+  user: process.env.DB_USER || 'avnadmin',
+  password: process.env.DB_PASSWORD || 'AVNS_MjGXx79jT1clBMpNaGS',
+  database: process.env.DB_NAME || 'defaultdb',
 });
 
 console.log('✅ Connected to MySQL DB');
